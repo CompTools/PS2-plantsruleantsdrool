2629d9be035d39e2be976824be7eee414a953009
jwilli81 <43003817+jwilli81@users.noreply.github.com>
2018-09-21 08:46:17 -0400

    Bonus_Miao_Version_JLW.sh
    Modified lines 35 and 41 to query for the airport code or city, state specified by the user, instead of "GNV".

--- Bonus_Miao_version.sh
+++ Bonus_Miao_version.sh
@@ -32,13 +32,13 @@ fi
 if [[ $REPLY =~ ^[A-Z]{3}$ ]]; then
   #echo -e "It's Airport Code: $REPLY"
   echo -e "Please wait ...\nThe Flights in and out of Gainesville from $REPLY is:"
-  cat ./data/flights.May2017-Apr2018.csv|grep GNV |grep $REPLY|cut -f13,16 -d','|grep "1"|wc -l
+  grep -w "$REPLY" ./data/flights.May2017-Apr2018.csv | cut -d "," -f 13,16 | grep -c 1.00
 elif [[ $REPLY =~ ^[A-Z].*[A-Z]{2}$ ]]; then
   #echo -e "City,State Name: $REPLY"
   Query=$(echo "$REPLY"|sed 's/^/"/g;s/$/"/g')
   #echo $Query
   echo -e "Please wait ...\nThe Flights in and out of Gainesville from $REPLY is:"
-  cat ./data/flights.May2017-Apr2018.csv|grep GNV |grep "$Query"|cut -f13,16 -d','|grep "1"|wc -l
+  grep -w "$REPLY" ./data/flights.May2017-Apr2018.csv | cut -d "," -f 13,16 | grep -c 1.00
 else
   invalid_input >&2
   exit 1
